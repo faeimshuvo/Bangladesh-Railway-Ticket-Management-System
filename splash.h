ebb715becb1be2dd11acbaf55666ac62dbd34297
faeimshuvo <137646945+faeimshuvo@users.noreply.github.com>
2023-06-25 00:18:30 +0600

    Add files via upload

---
+++ splash.h
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include<dos.h>
+
+
+
+
+void showSplashScreen() {
+
+printf("                              .  .:=+=:           \n");
+printf("                     . .-#+...   :   -  -:        \n");
+printf("               .-+:::           .   ::: . -       \n");
+printf("         -.-=   .:. .-= <BR> =-: :  :     :.      \n");
+printf("    :.  ::.:::..::=+ WELCOME TO  +=  =  :   .     \n");
+printf("   ..   . .::::   :  BANGLADESH   :  #. .  :.     \n");
+printf("   :  ........:. :+=:  RAILWAY  :=  ..-*..:..     \n");
+printf("      :..  . :  .  . : ::  :.  :.   : =- .  .-    \n");
+printf("          .:* . .    .........  :. . : := : .:    \n");
+printf("              +:.   :                .   + - -    \n");
+printf("                      .*  =   .   =   - .  :. -   \n");
+printf("                            *..=  .   _ _   . =   \n");
+printf("                                  +.::..:====..   \n");
+
+
+}
